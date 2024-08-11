7a0bbcfbbb9fa5b91f45a12b3cd0e418f2e9aa65
Calvin Le <email.calvinle@gmail.com>
2024-08-10 18:07:31 -0700

    updated privacy policy

--- src/components/Privacy.tsx
+++ src/components/Privacy.tsx
@@ -3,34 +3,73 @@ import React from "react";
 const Privacy: React.FC = () => {
   return (
     <div className="mx-24 mb-10">
-      <p>Effective Date: July 27th, 2024</p>
-      <p>
+      <p>Effective Date: August 10th, 2024</p>
+      <p className="my-5">
         Welcome to retireinbitcoin.com ("we", "our", "us"). We are committed to
         protecting your privacy and ensuring that your personal information is
         handled in a safe and responsible manner. This Privacy Policy outlines
         how we collect, use, and protect your information when you visit our
         website.
       </p>
-      <p className="font-bold mt-5">Information We Collect:</p>
+      <p className="font-bold mt-5">Consent:</p>
+      <p className="my-5">
+        By using our website, you hereby consent to our Privacy Policy and agree
+        to its terms.
+      </p>
       <ul className="list-disc list-inside my-5">
         <li>
-          <span className="font-bold">Personal Information:</span> We may
-          collect personal information such as your name, email address, and any
-          other information you voluntarily provide to us.
+          <span className="font-bold">Personal Information:</span> We do not
+          collect your browsing data nor personal data (i.e. name, email
+          address, phone number). We do not share your data directly with
+          anyone. The services we use may collect and use your data anonymously
+          directly.
         </li>
         <li>
-          <span className="font-bold">Usage Data:</span> We collect information
-          on how you interact with our website, including but not limited to
-          your IP address, browser type, pages visited, and the time and date of
-          your visit.
+          <span className="font-bold">Usage Data:</span> We do not collect
+          information on how you interact with our website, including but not
+          limited to your IP address, browser type, pages visited, and the time
+          and date of your visit.
         </li>
         <li>
           <span className="font-bold">Cookies and Tracking Technologies:</span>{" "}
-          We use cookies and similar tracking technologies to track the activity
-          on our website and hold certain information.
+          {/* We use cookies and similar tracking technologies to track the activity
+          on our website and hold certain information. */}
+          retireinbitcoin.com follows a standard procedure of using log files.
+          These files log visitors when they visit websites. All hosting
+          companies do this and a part of hosting services' analytics. The
+          information collected by log files include internet protocol (IP)
+          addresses, browser type, Internet Service Provider (ISP), date and
+          time stamp, referring/exit pages, and possibly the number of clicks.
+          These are not linked to any information that is personally
+          identifiable. The purpose of the information is for analyzing trends,
+          administering the site, tracking users' movement on the website, and
+          gathering demographic information.
+        </li>
+        <li>
+          <span className="font-bold">Web Beacons</span> {""}
+          Like any other website, retireinbitcoin.com uses 'cookies'. These
+          cookies are used to store information including visitors' preferences,
+          and the pages on the website that the visitor accessed or visited. The
+          information is used to optimize the users' experience by customizing
+          our web page content based on visitors' browser type and/or other
+          information.
+        </li>
+        <li>
+          <span className="font-bold">Google DoubleClick DART Cookie</span> {""}
+          Google is one of a third-party vendor on our site. It also uses
+          cookies, known as DART cookies, to serve ads to our site visitors.
+          However, visitors may choose to decline the use of DART cookies by
+          visiting the Google ad and content network Privacy Policy at the
+          following URL -{" "}
+          <a
+            href="https://policies.google.com/technologies/ads"
+            className="text-blue-500"
+          >
+            https://policies.google.com/technologies/ads
+          </a>
         </li>
       </ul>
-      <p className="font-bold mt-5">How We Use Your Information:</p>
+      {/* <p className="font-bold mt-5">How We Use Your Information:</p>
       <ul className="list-disc list-inside my-5">
         <li>
           <span className="font-bold">
@@ -53,7 +92,7 @@ const Privacy: React.FC = () => {
           display personalized advertisements through Google AdSense and to
           inform you about promotions and offers.
         </li>
-      </ul>
+      </ul> */}
       <p className="font-bold mt-5">Google AdSense:</p>
       <p className="my-5">
         We use Google AdSense to serve ads on our website. Google AdSense may
@@ -62,11 +101,7 @@ const Privacy: React.FC = () => {
         information is used to provide personalized advertisements tailored to
         your interests. For more information on how Google AdSense collects and
         uses data, please visit the{" "}
-        <a
-          href="https://policies.google.com/privacy"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://policies.google.com/privacy" className="text-blue-500">
           Google AdSense Privacy Policy
         </a>
         .
@@ -103,7 +138,7 @@ const Privacy: React.FC = () => {
         Internet or electronic storage is 100% secure, and we cannot guarantee
         absolute security.
       </p>
-      <p className="font-bold mt-5">Your Rights:</p>
+      {/* <p className="font-bold mt-5">Your Rights:</p>
       <ul className="list-disc list-inside my-5">
         <li>
           <span className="font-bold">Access Your Information:</span> Request a
@@ -117,13 +152,17 @@ const Privacy: React.FC = () => {
           <span className="font-bold">Delete Your Information:</span> Request
           deletion of your personal information, subject to certain exceptions.
         </li>
-      </ul>
+      </ul> */}
       <p className="font-bold mt-5">Third-Party Links:</p>
       <p className="my-5">
-        Our website may contain links to third-party websites. We are not
-        responsible for the privacy practices or the content of these websites.
-        We encourage you to review the privacy policies of any third-party
-        websites you visit.
+        Our Privacy Policy does not apply to other advertisers or websites.
+        Thus, we are advising you to consult the respective Privacy Policies of
+        these third-party ad servers for more detailed information. It may
+        include their practices and instructions about how to opt-out of certain
+        options. You can choose to disable cookies through your individual
+        browser options. To know more detailed information about cookie
+        management with specific web browsers, it can be found at the browsers'
+        respective websites.
       </p>
       <p className="font-bold mt-5">Changes to This Privacy Policy:</p>
       <p className="my-5">
@@ -136,7 +175,7 @@ const Privacy: React.FC = () => {
         If you have any questions about this Privacy Policy, please contact us
         at: btcretire@protonmail.com
       </p>
-      <p>By using our website, you consent to our Privacy Policy.</p>
+      {/* <p>By using our website, you consent to our Privacy Policy.</p> */}
     </div>
   );
 };
